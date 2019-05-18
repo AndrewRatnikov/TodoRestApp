f52acfc3ed3c41feb009034b60e58df6695fa81c
Andrew Ratnikov <ratnikov.am@gmail.com>
2019-05-18 11:56:58 +0300

    reorganize folders

--- app/index.js
+++ app/index.js
@@ -7,7 +7,12 @@ import todoRoutes from "./routes";
 
 const app = express();
 
-mongoose.connect(config.DB, { useNewUrlParser: true, dbName: "todos" });
+mongoose
+  .connect(config.DB, { useNewUrlParser: true, dbName: "todos" })
+  .then(() => console.log("DB connect"))
+  .catch(err => console.log(`error in DB connection: ${err}`));
+
+mongoose.Promise = global.Promise;
 
 // parse application/x-www-form-urlencoded
 app.use(bosyParser.urlencoded({ extended: true }));
@@ -15,10 +20,10 @@ app.use(bosyParser.urlencoded({ extended: true }));
 app.use(bosyParser.json());
 
 // Routes
+app.use("/todo", todoRoutes);
 app.get("/", (request, response) =>
   response.status(200).json({ working: true })
 );
-app.use("/todo", todoRoutes);
 
 app.listen(config.APP_PORT, () => {
   console.log(`Server is running on port ${config.APP_PORT}`);
